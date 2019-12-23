66b3defba13c3674a2afb2b4f878ac93abbad8a6
蒼時弦也 <elct9620@frost.tw>
2019-12-23 16:04:19 +0800

    Initialize Project

---
+++ spec/mitake_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mitake do
+  it 'has a version number' do
+    expect(Mitake::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
